160cc231fcba7dcc36a64c3f966f7a0b0e706e08
Minhtrong360 <nguyenminhtrong402@gmail.com>
2022-06-20 22:48:59 +0700

    1st update

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  let color = count > 0 ? "green" : count < 0 ? "red" : "black";
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
+
+  return (
+    <div className="container">
+      <h1> Counter </h1>
+      <span style={{ color }} id="value">
+        {count}
+      </span>
+      <div className="btn-group">
+        <button onClick={decrement} className="btn btn-decrease">
+          DECREASE
+        </button>
+        <button onClick={reset} className="btn btn-reset">
+          RESET
+        </button>
+        <button onClick={increment} className="btn btn-increase">
+          INCREASE
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
